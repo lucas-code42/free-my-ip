360e2badfd0fe3f357b4a65385d4f6f0962a216a
Lucas Brandão <lucas.brandao@qyon.com>
2022-05-16 21:58:32 -0300

    add new proxies list and requirements

--- proxy.py
+++ proxy.py
@@ -1,26 +1,92 @@
 import requests
 from bs4 import BeautifulSoup as BSHTML
 
-def get_proxys():
-    try:
-        url = 'https://free-proxy-list.net/'
-        session = requests.session()
-        response = session.get(url)
-        if response.status_code == 200:
-            html = response.text
-            proxies = []
-            soup = BSHTML(html, 'html.parser')
-            for row in soup.find('table', attrs={'class': 'table table-striped table-bordered'}).find_all('tr')[1:]:
-                td = row.find_all('td')
-                try:
-                    ip = td[0].text.strip()
-                    port = td[1].text.strip()
-                    proxies.append(str(ip) + ':' + str(port))
-                except IndexError:
-                    continue
-            return proxies
-    except:
-        return False
+class FreeMyIp:
+
+    def get_proxies(self, ):
+        try:
+            url = 'https://free-proxy-list.net/'
+            session = requests.session()
+            response = session.get(url)
+            if response.status_code == 200:
+                html = response.text
+                proxies = []
+                soup = BSHTML(html, 'html.parser')
+                for row in soup.find('table', attrs={'class': 'table table-striped table-bordered'}).find_all('tr')[1:]:
+                    td = row.find_all('td')
+                    try:
+                        ip = td[0].text.strip()
+                        port = td[1].text.strip()
+                        proxies.append(str(ip) + ':' + str(port))
+                    except IndexError:
+                        continue
+                return proxies
+        except:
+            return False
+    
+    def get_socks_proxies(self, ):
+        try:
+            url = 'https://www.socks-proxy.net/'
+            session = requests.session()
+            response = session.get(url)
+            if response.status_code == 200:
+                html = response.text
+                proxies = []
+                soup = BSHTML(html, 'html.parser')
+                for row in soup.find('table', attrs={'class': 'table table-striped table-bordered'}).find_all('tr')[1:]:
+                    td = row.find_all('td')
+                    try:
+                        ip = td[0].text.strip()
+                        port = td[1].text.strip()
+                        proxies.append(str(ip) + ':' + str(port))
+                    except IndexError:
+                        continue
+                return proxies
+        except:
+            return False
+
+    def get_us_proxies(self, ):
+        try:
+            url = 'https://www.us-proxy.org/'
+            session = requests.session()
+            response = session.get(url)
+            if response.status_code == 200:
+                html = response.text
+                proxies = []
+                soup = BSHTML(html, 'html.parser')
+                for row in soup.find('table', attrs={'class': 'table table-striped table-bordered'}).find_all('tr')[1:]:
+                    td = row.find_all('td')
+                    try:
+                        ip = td[0].text.strip()
+                        port = td[1].text.strip()
+                        proxies.append(str(ip) + ':' + str(port))
+                    except IndexError:
+                        continue
+                return proxies
+        except:
+            return False
+
+    def get_uk_proxies(self, ):
+        try:
+            url = 'https://free-proxy-list.net/uk-proxy.html'
+            session = requests.session()
+            response = session.get(url)
+            if response.status_code == 200:
+                html = response.text
+                proxies = []
+                soup = BSHTML(html, 'html.parser')
+                for row in soup.find('table', attrs={'class': 'table table-striped table-bordered'}).find_all('tr')[1:]:
+                    td = row.find_all('td')
+                    try:
+                        ip = td[0].text.strip()
+                        port = td[1].text.strip()
+                        proxies.append(str(ip) + ':' + str(port))
+                    except IndexError:
+                        continue
+                return proxies
+        except:
+            return False
+
 
 
 """
@@ -56,7 +122,6 @@ def selenium():
     sleep(1000)
 
 selenium()
-
 
 ########################################
 MODELO DE IMPLEMENTAÇÃO PARA REQUESTS #
---
+++ test.py
@@ -0,0 +1,13 @@
+from proxy import FreeMyIp
+
+
+if __name__ == '__main__':
+    proxys = FreeMyIp()
+
+    test_1 = proxys.get_socks_proxies()
+    test_2 = proxys.get_proxies()
+    test_3 = proxys.get_us_proxies()
+
+    print(test_1)
+    print(test_2)
+    print(test_3)
